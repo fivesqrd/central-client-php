9ed7474923c78a5b7542d0f077de4a3811f0eccd
Christian <christian@fivesqrd.com>
2018-05-17 12:13:33 +0200

    Fixed config nesting bug

--- src/Laravel/Config.php
+++ src/Laravel/Config.php
@@ -1,24 +1,20 @@
 <?php
 
 return [
-
-    'central' => [
-        'namespace' => env('CENTRAL_NAMESPACE'),
-        'adapter'   => env('CENTRAL_ADAPTER', 'Aws'), // Aws|Bego
-        'options'   => [ // Adapter specific options
-            'table'     => env('CENTRAL_TABLE'), // Required by both Bego and Aws
-            'client'    => null,
-            'marshaler' => null
-        ],
-        'aws'       => [
-            'version' => env('AWS_VERSION', '2012-08-10'),
-            'region'  => env('AWS_REGION', 'eu-west-1'),
-            'credentials' => [
-                'key'    => env('AWS_KEY'),
-                'secret' => env('AWS_SECRET'),
-            ],
-            'endpoint' => env('AWS_ENDPOINT') ?: null
-        ], 
+    'namespace' => env('CENTRAL_NAMESPACE'),
+    'adapter'   => env('CENTRAL_ADAPTER', 'Aws'), // Aws|Bego
+    'options'   => [ // Adapter specific options
+        'table'     => env('CENTRAL_TABLE'), // Required by both Bego and Aws
+        'client'    => null,
+        'marshaler' => null
     ],
-
+    'aws'       => [
+        'version' => env('AWS_VERSION', '2012-08-10'),
+        'region'  => env('AWS_REGION', 'eu-west-1'),
+        'credentials' => [
+            'key'    => env('AWS_KEY'),
+            'secret' => env('AWS_SECRET'),
+        ],
+        'endpoint' => env('AWS_ENDPOINT') ?: null
+    ], 
 ];
